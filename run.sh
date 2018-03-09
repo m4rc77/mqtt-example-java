18922f25736e588b34c08713155297739bcc20f4
Marc Schmid (mschmid) <mschmid@puzzle.ch>
2018-03-09 21:12:58 +0100

    Added run.sh script

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java -cp "$HOME/.m2/repository/org/eclipse/paho/org.eclipse.paho.client.mqttv3/1.2.0/org.eclipse.paho.client.mqttv3-1.2.0.jar:target/mqtt-example-java-1.0-SNAPSHOT.jar" ch.m4rc77.mqttexample.MqttExample /puzzle/javakafi
